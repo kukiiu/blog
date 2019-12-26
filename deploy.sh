7d8be1cf2eee306b0265dea979c8efcd435b692e
kukiiu <ex-quyang001@pingan.com.cn>
2019-12-26 23:46:41 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,20 @@
 # 终止一个错误
 set -e
 
+# 构建
+npm run build
+
 git add -A
 git commit -m 'deploy'
 git push -f git@github.com:kukiiu/blog.git master
+
+# 进入生成的构建文件夹
+cd blog/.vuepress/dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:kukiiu/blog.git master:gh-pages
 
 cd -
